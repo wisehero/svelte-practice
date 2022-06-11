b9b6b146b48bf524c1cd8c7d2546fa40847b4fae
wisehero10 <godjw96@gmail.com>
2022-06-12 01:08:45 +0900

    # Component 기본 사용

---
+++ src/chap03/code3-1_3-7/App.svelte
@@ -0,0 +1,10 @@
+<script>
+    import Header from "header.svelte";
+    import Content from "content.svelte";
+    import Footer from "footer.svelte";
+</script>
+<Header/>
+<Content/>
+<Content/>
+<Footer/>
+<Footer/>
