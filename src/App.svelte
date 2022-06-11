9339d170a6754c076706a9dddaffad370155fe1d
wisehero10 <godjw96@gmail.com>
2022-06-12 02:28:23 +0900

    # Props Event 2

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,8 @@
 <script>
-    import Child from './chap03/code3-25_3-26/child.svelte';
+    import Middle from './chap03/code3-32_3-34/middle.svelte';
 
     let count = 0;
 
     const handleClick = () => count = count + 1;
 </script>
-<Child carryValue={count}/>
-<button on:click={handleClick}>클릭</button>
+<Middle {count} {handleClick}></Middle>
---
+++ src/chap03/code3-28_3-29/child.svelte
@@ -0,0 +1,6 @@
+<script>
+    export let carryValue;
+    export let handleClick;
+</script>
+<p>전달된 값은 { carryValue }</p>
+<button on:click={handleClick}>클릭</button>
