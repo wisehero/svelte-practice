e035747fef57cad0d4be6eae19cee2544472ff75
wisehero10 <godjw96@gmail.com>
2022-06-19 19:59:40 +0900

    # bind:group과 반복을 사용해서 값 바인딩

---
+++ src/chap04/code4-13/App.svelte
@@ -0,0 +1,15 @@
+<script>
+    let numbers = [
+        'one',
+        'two',
+        'three'
+    ]
+
+    let getNumbers = []
+</script>
+{#each numbers as number}
+    <label>
+        <input type="checkbox" bind:group={getNumbers} value={number}>
+    </label>
+{/each}
+{getNumbers}
