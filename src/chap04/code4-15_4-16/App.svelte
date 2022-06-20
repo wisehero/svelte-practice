e43fa8a805055389b6a8c362638ef728b758240a
wisehero10 <godjw96@gmail.com>
2022-06-20 22:48:57 +0900

    # 컴포넌트 바인딩

---
+++ src/chap04/code4-15_4-16/App.svelte
@@ -0,0 +1,8 @@
+<script>
+    import Increment from './increment.svelte'
+
+    let value = 0
+</script>
+<input type="Number" bind:value={value}>
+<h2>{value}</h2>
+<Increment bind:count={value}></Increment>
---
+++ src/chap04/code4-15_4-16/increment.svelte
@@ -0,0 +1,6 @@
+<script>
+    export let count
+
+    const increNum = () => count = Number(count) + 1;
+</script>
+<button on:click={increNum}>Increment</button>
