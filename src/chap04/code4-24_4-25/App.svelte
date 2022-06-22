b601aa9a2e47a862080b3f53f8d710dfca3b38e5
wisehero10 <godjw96@gmail.com>
2022-06-22 23:00:35 +0900

    # 컴포넌트 라이프사이클

---
+++ src/chap04/code4-24_4-25/App.svelte
@@ -0,0 +1,19 @@
+<script>
+    import {afterUpdate, beforeUpdate, onDestroy, onMount} from 'svelte';
+    import Child from './child.svelte';
+
+    onMount(async () => {
+        console.log('App onMount');
+    });
+    onDestroy(async () => {
+        console.log('App onDestroy');
+    });
+    beforeUpdate(async () => {
+        console.log('App beforeUpdate');
+    });
+    afterUpdate(async () => {
+        console.log('App afterUpdate');
+    });
+</script>
+<h1>부모 컴포넌트</h1>
+<Child/>
---
+++ src/chap04/code4-24_4-25/child.svelte
@@ -0,0 +1,20 @@
+<script>
+    import {afterUpdate, beforeUpdate, onDestroy, onMount} from 'svelte';
+
+    onMount(async () => {
+        console.log('child onMount')
+    })
+
+    onDestroy(async () => {
+        console.log('child onDestroy')
+    })
+
+    beforeUpdate(async () => {
+        console.log('child beforeUpdate')
+    })
+
+    afterUpdate(async () => {
+        console.log("child afterUpdate")
+    })
+</script>
+<p>자식 컴포넌트</p>
